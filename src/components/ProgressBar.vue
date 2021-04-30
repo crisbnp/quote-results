d89497f33d0a4ca95c18c91ccfc3cd51d892ef99
cristien natal <crisbnp@icloud.com>
2021-04-30 20:58:21 +0100

    change progress tab to multistep form stepper

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,48 +1,51 @@
 <template>
-  <v-tabs background-color="grey darken-3" color="white" centered>
-    <v-tab class="white--text">
-      <v-progress-circular
-        :width="1"
-        :value="100"
-        color="red"
-      ><span class="white--text">1</span></v-progress-circular>
+<div class="bg-dark">
+  <v-stepper non-linear dark>
+    <v-stepper-header>
+      <v-stepper-step
+        editable
+        step="1"
+        color="#D1D834"
+      >
         Personal Details
-    </v-tab>
-    <v-tab class="white--text">
-      <v-progress-circular
-    :width="1"
-      :value="100"
-      color="red"
-    ><span class="white--text">2</span></v-progress-circular>
-    Property Details
-    </v-tab>
-    <v-tab class="white--text">
-    <v-progress-circular
-    :width="1"
-      :value="100"
-      color="red"
-    ><span class="white--text">3</span></v-progress-circular>
-    Cover Details
-    </v-tab>
+      </v-stepper-step>
+      
+      <v-stepper-step
+        editable
+        step="2"
+        color="#D1D834"
+      >
+        Property Details
+      </v-stepper-step>
 
-    <v-tab class="white--text">
-    <v-progress-circular
-    :width="1"
-      :value="100"
-      color="red"
-    ><span class="white--text">4</span></v-progress-circular>
-    Previous Claims
-    </v-tab>
-    <v-tab class="white--text">
-    <v-progress-circular
-    :width="1"
-      :value="100"
-      color="#D1D834"
-      class="activestate"
-    ><span>5</span></v-progress-circular>
-    Results
-    </v-tab>
-  </v-tabs>
+      <v-stepper-step
+        editable
+        step="3"
+        color="#D1D834"
+      >
+        Cover Details
+      </v-stepper-step>
+
+      <v-stepper-step
+        editable
+        step="4"
+        color="#D1D834"
+      >
+        Previous Claims
+      </v-stepper-step>
+
+      <v-stepper-step
+        editable
+        step="5"
+        color="#D1D834"
+      >
+        Results
+      </v-stepper-step>
+
+
+    </v-stepper-header>
+  </v-stepper>
+</div>
 </template>
 
 <script>
@@ -51,17 +54,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.v-progress-circular {
-  margin: 1rem;
+<style lang="scss">
+.bg-dark {
+  background-color: #2C3536;
 }
 
-.activestate {
-  background-color: #D1D834;
+.v-stepper__step__step {
   border-radius: 50%;
-
-  span {
-    color: black;
-  }
+  border: 1px solid red;
+  background: rgba(209,216,52,0.1) !important;
 }
+
+
 </style>
